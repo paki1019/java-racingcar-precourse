19e89066f2bc7946d63e0f89bf153235fd622257
paki1019 <paki1019@gmail.com>
2022-10-07 17:30:33 +0900

    feat : 시도할 횟수 입력 받기 - 입력 실패시 재시도

--- src/main/java/racingcar/view/InputRounds.java
+++ src/main/java/racingcar/view/InputRounds.java
@@ -5,10 +5,24 @@ import racingcar.model.Rounds;
 
 public class InputRounds {
 	private Rounds rounds;
+	private boolean retry;
 
 	public Rounds readRounds() {
-		System.out.println("시도할 횟수는 몇회인가요?");
-		String input = Console.readLine();
-		return Rounds.generate(input);
+		do {
+			tryReadRounds();
+		} while (retry);
+		return this.rounds;
+	}
+
+	private void tryReadRounds() {
+		try {
+			System.out.println("시도할 횟수는 몇회인가요?");
+			String input = Console.readLine();
+			this.rounds = Rounds.generate(input);
+			this.retry = false;
+		} catch (IllegalArgumentException e) {
+			System.out.println("[ERROR] " + e.getMessage());
+			this.retry = true;
+		}
 	}
 }
