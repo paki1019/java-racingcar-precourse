9a04321b01bfb498e432ccf8a242595f6afa7a7e
paki1019 <paki1019@gmail.com>
2022-10-07 17:09:28 +0900

    feat : 시도할 횟수 입력 받기 - 시도할 횟수 음수, 0

--- src/main/java/racingcar/model/Rounds.java
+++ src/main/java/racingcar/model/Rounds.java
@@ -1,10 +1,18 @@
 package racingcar.model;
 
 public class Rounds {
+	private static final int ZERO = 0;
 	private final int rounds;
 
 	private Rounds(String rounds) {
+		validate(rounds);
 		this.rounds = Integer.parseInt(rounds);
+	}
+
+	private void validate(String rounds) {
+		if (Integer.parseInt(rounds) <= ZERO) {
+			throw new IllegalArgumentException("시도 횟수는 양수여야 합니다.");
+		}
 	}
 
 	public static Rounds generate(String input) {
--- src/test/java/racingcar/model/RoundsTest.java
+++ src/test/java/racingcar/model/RoundsTest.java
@@ -13,10 +13,29 @@ class RoundsTest {
 
 	@DisplayName("시도할 횟수 문자열 입력")
 	@ParameterizedTest
-	@ValueSource(strings = {"1", "2", "3"})
+	@ValueSource(strings = {"1", "2", "3", "04", "05"})
 	void generate(String input) {
 		Rounds rounds = Rounds.generate(input);
 		assertThat(rounds).isNotNull();
 		assertThat(rounds.getRounds()).isEqualTo(Integer.parseInt(input));
+	}
+
+	@DisplayName("시도할 횟수 문자열 입력 - 0")
+	@ParameterizedTest
+	@ValueSource(strings = {"0", "00", "000"})
+	void generate_zero(String input) {
+		assertThatThrownBy(() -> Rounds.generate(input))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("시도 횟수는 양수여야 합니다.");
+	}
+
+
+	@DisplayName("시도할 횟수 문자열 입력 - 음수")
+	@ParameterizedTest
+	@ValueSource(strings = {"-1", "-002", "-999"})
+	void generate_negative(String input) {
+		assertThatThrownBy(() -> Rounds.generate(input))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("시도 횟수는 양수여야 합니다.");
 	}
 }
